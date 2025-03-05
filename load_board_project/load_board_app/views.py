a56ec48c73c2abf965dde5ce07e58d6af002713a
Drew-Andersen <andrewtandersen@gmail.com>
2025-03-05 11:57:41 -0500

    Working on scraping

--- load_board_project/load_board_app/views.py
+++ load_board_project/load_board_app/views.py
@@ -3,4 +3,4 @@ from .scraper import scrape_load_board
 
 def load_board_view(request):
     load_listings = scrape_load_board()
-    return render(request, 'load_board_app/load_list.html', { 'load_listings': load_listings })
+    return render(request, 'load_board_app/load_list.html', { 'load_listings': load_listings }) 
